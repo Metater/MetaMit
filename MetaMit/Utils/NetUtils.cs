6808eaee6cd5f4b55396e5cb6ee07c5b9652cf8b
Metater <metaterplays@gmail.com>
2021-01-04 17:03:20 -0500

    Added TryGetIP input is an AddressFamily

--- MetaMit/Utils/NetUtils.cs
+++ MetaMit/Utils/NetUtils.cs
@@ -18,9 +18,12 @@ namespace MetaMit.Utils
         {
             return new IPEndPoint(ipAddress, port);
         }
-        public static IPEndPoint GetLocalEndPoint(int port)
+        public static IPEndPoint GetLocalEndPoint(int port, bool ipv4)
         {
-            return new IPEndPoint(GetIP(), port);
+            if (ipv4)
+                return new IPEndPoint(GetLocalIPv4(), port);
+            else
+                return new IPEndPoint(GetLocalIPv6(), port);
         }
         public static IPAddress GetLocalIPv4()
         {
@@ -33,6 +36,20 @@ namespace MetaMit.Utils
                 }
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        public static bool TryGetIP(AddressFamily addressFamily, out IPAddress ipAddress)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == addressFamily)
+                {
+                    ipAddress = ip;
+                    return true;
+                }
+            }
+            ipAddress = default(IPAddress);
+            return false;
         }
         public static IPAddress GetLocalIPv6()
         {
