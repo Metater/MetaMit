2317808d4f5653278fcf879b267eb090e67ef239
Connor <61102529+Metater@users.noreply.github.com>
2021-05-03 17:04:11 -0400

    Add code from school

--- MetaMitStandard/Utils/DataUnpackerNew.cs
+++ MetaMitStandard/Utils/DataUnpackerNew.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace MetaMitStandard.Utils
 {
@@ -11,16 +12,70 @@ namespace MetaMitStandard.Utils
         private ushort sessionFlags = 0;
         private int unreadData = -1;
 
-        public bool TryParseData(int bytesReceived, byte[] data, out List<byte[]> unpackedData, out ushort sessionFlags)
+        public bool TryUnpackData(int bytesReceived, byte[] data, out List<byte[]> unpackedData, out ushort sessionFlags)
         {
-            Console.WriteLine("Received Data: ");
-            foreach(byte b in data)
-            {
-                Console.WriteLine("\t" + b);
-            }
+            int unreadBufferData = -1;
+            unreadBufferData = bytesReceived;
             unpackedData = new List<byte[]>();
+
+            while (unreadBufferData > 0) // Done reading entire buffer
+            {
+                if (unreadData == -1) // Ready to start reading a new packet
+                {
+                    dataLength = BitConverter.ToUInt16(data, bytesReceived - unreadBufferData);
+                    unreadBufferData -= 2;
+                    sessionFlags = BitConverter.ToUInt16(data, bytesReceived - unreadBufferData);
+                    unreadBufferData -= 2;
+
+                    unreadData = dataLength;
+                }
+                else // Continue reading data from a packet
+                {
+
+                }
+                int readableData = Math.Min(unreadData, unreadBufferData);
+                byte[] dataSegment = new byte[readableData];
+                Buffer.BlockCopy(data, bytesReceived - unreadBufferData, dataSegment, 0, readableData);
+                dataSegments.Add(dataSegment);
+                unreadBufferData -= readableData;
+                unreadData -= readableData;
+                Console.WriteLine(unreadBufferData);
+                Console.WriteLine(unreadData);
+                if (unreadData == 0) // Done reading a packet
+                {
+                    unpackedData.Add(CombineSegments());
+                    dataSegments.Clear();
+                    unreadData = -1;
+                }
+            }
+
+
+
+
+
+            if (data.Length != bytesReceived) // The buffer only contains a packet or packets that are not partial
+            {
+
+            }
+            else // Buffer contains packets and a partial packet, or a partial packet
+            {
+
+            }
             sessionFlags = this.sessionFlags;
-            return false;
+            return unpackedData.Count != 0;
+        }
+
+        private byte[] CombineSegments()
+        {
+            byte[][] arrays = dataSegments.ToArray();
+            byte[] combinedArray = new byte[arrays.Sum(a => a.Length)];
+            int offset = 0;
+            foreach (byte[] array in arrays)
+            {
+                Buffer.BlockCopy(array, 0, combinedArray, offset, array.Length);
+                offset += array.Length;
+            }
+            return combinedArray.ToArray();
         }
     }
 }
