5508b5131eae676a315ad5ea09034d0a9031a562
Connor <61102529+Metater@users.noreply.github.com>
2021-05-06 22:57:15 -0400

    Add data packer method

--- MetaMitStandard/Utils/DataPacker.cs
+++ MetaMitStandard/Utils/DataPacker.cs
@@ -6,5 +6,15 @@ namespace MetaMitStandard.Utils
 {
     public class DataPacker
     {
+        public static byte[] PackData(byte[] data, ushort dataSessionFlags)
+        {
+            byte[] length = BitConverter.GetBytes((ushort)data.Length);
+            byte[] sessionFlags = BitConverter.GetBytes(dataSessionFlags);
+            byte[] packedData = new byte[length.Length + sessionFlags.Length + data.Length];
+            Buffer.BlockCopy(length, 0, packedData, 0, length.Length);
+            Buffer.BlockCopy(sessionFlags, 0, packedData, length.Length, sessionFlags.Length);
+            Buffer.BlockCopy(data, 0, packedData, length.Length + sessionFlags.Length, data.Length);
+            return packedData;
+        }
     }
 }
